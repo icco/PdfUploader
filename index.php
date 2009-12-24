b4623742987398e36fa7941a55c8bdcad4b6ebf4
Nat Welch <nat@natwelch.com>
2009-12-23 19:43:01 -0800

    adding new files

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+   <head>
+      <title>Cal Poly ACM Resume Book</title>
+   </head>
+   <body>
+      <form method="post" enctype="multipart/form-data" action="processPDF.php">
+         <label for="fname">First Name: </label> 
+         <input name="fname" id="fname">
+         <label for="lname">Last Name: </label> 
+         <input name="lname" id="lname">
+         <label for="id_file">File: </label> 
+         <input name="file" id="id_file" type="file">
+         <button type="submit" class="bigbutton">Upload</button>
+      </form>
+   </body>
+</html>
+
---
+++ processPDF.php
@@ -0,0 +1,7 @@
+<?php
+require "config.php"
+
+class PDFUpload {
+
+}
+?>
