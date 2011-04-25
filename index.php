ea90b962d79982e85109562ac8fc8dfb7a1568f8
Nat Welch <nat@natwelch.com>
2011-04-25 14:42:13 -0700

    whitespace.

--- index.php
+++ index.php
@@ -25,28 +25,28 @@ if (isset($_POST['fname']) &&
             margin-left: auto;
             margin-right: auto;
          }
-         
+
          table {
             width: 450px;
             margin-left: auto;
             margin-right: auto;
          }
-         
+
          h1 {
             padding: 5px 0;
             font-family: sans-serif;
          }
-         
+
          h2 {
             margin-top: 5px;
             margin-bottom: 60px;
          }
-         
+
          #yay {
             background-color: LightGreen;
             border-color: DarkGreen;
          }
-         
+
          .notify {
             padding: 15px;
             margin-bottom: 20px;
@@ -57,7 +57,7 @@ if (isset($_POST['fname']) &&
             background-color: LightCoral;
             border-color: DarkRed;
          }
-         
+
          label {
             font-weight: 900;
             font-family: serif;
@@ -65,8 +65,8 @@ if (isset($_POST['fname']) &&
       -->
       </style>
       <!--
-         This page was coded by Nathaniel "Nat" Welch, for Cal Poly's 
-         Association of Computing Machinery. The source code is available at 
+         This page was coded by Nathaniel "Nat" Welch, for Cal Poly's
+         Association of Computing Machinery. The source code is available at
          http://github.com/icco/PdfUploader.
       -->
    </head>
@@ -79,21 +79,21 @@ if (isset($_POST['fname']) &&
             <p>
                Thank you for uploading your resume <?php print "$fname $lname"; ?>.
             </p>
-            <p> 
-               The resume you submitted is <a href="<?php echo $result; ?>">available for viewing</a> 
-               if you wish. If you do not like what you uploaded, you can overwrite 
+            <p>
+               The resume you submitted is <a href="<?php echo $result; ?>">available for viewing</a>
+               if you wish. If you do not like what you uploaded, you can overwrite
                it by putting in your name and uploading a new file.
-            </p> 
+            </p>
          </div>
       <?php } else if (isset($result) && $result === false) { ?>
          <div id="nay" class="notify">
             <h3>Failure!</h3>
             <p>
-               Either you did not upload a PDF, you are missing a field (First and Last name 
-               and your Major) or there is a problem with our servers. Both of your names need 
-               to be between 2 and 30 characters, with no spaces. Your Major, if you selected 
-               Other, must be all caps and not longer than four characters. If you did all of 
-               this, and still got this error, please wait a minute and try again. If 
+               Either you did not upload a PDF, you are missing a field (First and Last name
+               and your Major) or there is a problem with our servers. Both of your names need
+               to be between 2 and 30 characters, with no spaces. Your Major, if you selected
+               Other, must be all caps and not longer than four characters. If you did all of
+               this, and still got this error, please wait a minute and try again. If
                problems persist, please <a href="mailto:nwelch@calpoly.edu">email us</a>.
             </p>
          </div>
@@ -105,17 +105,17 @@ if (isset($_POST['fname']) &&
                <td><input name="fname" id="fname" style="width: 262px;"></td>
             </tr>
             <tr>
-               <td><label for="lname">Last Name: </label></td> 
+               <td><label for="lname">Last Name: </label></td>
                <td><input name="lname" id="lname" style="width: 262px;"></td>
             </tr>
             <tr>
                <td><label for="major">Major: </label></td>
                <td>
                   <select name="major" id="major" style="width: 262px;"
-                     onchange="javascript: 
-                        if (this.value == 'null') 
-                           $('OtherBox').setStyle('display', 'block'); 
-                        else 
+                     onchange="javascript:
+                        if (this.value == 'null')
+                           $('OtherBox').setStyle('display', 'block');
+                        else
                            $('OtherBox').setStyle('display', 'none');
                      ;" >
                      <option selected>CSC</option>
@@ -126,7 +126,7 @@ if (isset($_POST['fname']) &&
                   </select>
                </td>
                <td>
-                  <input type="text" name="OtherBox" id="OtherBox" 
+                  <input type="text" name="OtherBox" id="OtherBox"
                         style="display: none; width: 75px" maxlength="5" />
                </td>
             </tr>
@@ -136,7 +136,7 @@ if (isset($_POST['fname']) &&
             </tr>
             <tr>
                <td colspan="2">
-                  <button type="submit" class="bigbutton" 
+                  <button type="submit" class="bigbutton"
                    style="width: 105px; height: 60px; margin: 10px;">Upload</button>
                </td>
             </tr>
